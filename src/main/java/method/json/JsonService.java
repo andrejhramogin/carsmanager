8c961ff9345180b81cd85bfec8c5fa00f39e39c2
andrejhramogin <andrej.hramogin2@gmail.com>
2023-06-06 21:13:54 +0300

    3

--- src/main/java/method/json/JsonService.java
+++ src/main/java/method/json/JsonService.java
@@ -1,35 +1,31 @@
 package method.json;
 
 import car.Car;
-import car.CarListCreate;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import method.PrintList;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class JsonOutInPut {
+public class JsonService {
 
     //запись с помощью JSON в файл filename.json
-        public static void jsonWriteCarList(List<Car>list, String fileName) throws IOException {
+    public static void jsonWriteCarList(List<Car> list, String fileName) throws IOException {
 
-            ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
         try {
-            mapper.writeValue(new File(fileName+".json"), list);
+            mapper.writeValue(new File(fileName + ".json"), list);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-
-
     //чтение из файла filename.json List<Car>
     //В классе Car должен быть создан пустой конструктор.
     public static List<Car> jsonReadCarList(String fileName) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(new File(fileName + ".json"), new TypeReference<List<Car>>(){});
+        return mapper.readValue(new File(fileName + ".json"), new TypeReference<List<Car>>() {
+        });
     }
-
 }
