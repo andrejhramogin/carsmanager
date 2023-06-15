6c776a098c516f10ae9bbbc83117c237229c952e
andrejhramogin <andrej.hramogin2@gmail.com>
2023-06-15 20:09:15 +0300

    15/06/2023 - 1

--- src/main/java/org/cars/Main.java
+++ src/main/java/org/cars/Main.java
@@ -1,50 +1,30 @@
 package org.cars;
 
-import org.cars.model.Car;
-import org.cars.service.TxtInOutServiceImpl;
-import org.cars.utils.TestDataInitialisation;
-import org.cars.service.CarServiceImpl;
-import org.cars.utils.PrintListUtils;
-import org.cars.service.JsonInOutServiceImpl;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        CarServiceImpl carServiceImpl = new CarServiceImpl();
-        JsonInOutServiceImpl json = new JsonInOutServiceImpl();
-        TxtInOutServiceImpl txtService = new TxtInOutServiceImpl();
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String name = "postgres";
+        String password = "password";
 
-        List<Car> list = new ArrayList<>(TestDataInitialisation.createCarList());
-
-        //самые дорогие машины.
-        System.out.println("Most expensive cars:");
-        PrintListUtils.printCarList(carServiceImpl.findCarMaxPrice(list));
-        System.out.println();
-
-        //самые дешевые машины.
-        System.out.println("Most cheapest cars:");
-        PrintListUtils.printCarList(carServiceImpl.findCarMinPrice(list));
-
-//        JSON запись списка, отсортированного по бренду.
-        json.writeCarList(carServiceImpl.createCarListByBrand(json.readCarList("cars"), "toyota"), "toyota");
-        System.out.println();
-
-//        Чтение из файла toyota.json
-        System.out.println("Read List<Car> \"toyota\" from JSON file: ");
-        PrintListUtils.printCarList(json.readCarList("toyota"));
-
-
-        //Запись списка в файл .txt
-        txtService.writeCarList(list, "cars");
-
-        //Чтение списка из файла .txt
-        System.out.println();
-        System.out.println("List<Car> from file .txt");
-        PrintListUtils.printCarList(txtService.readCarList("cars"));
-
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(url, name, password);
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("Select * From user WHERE id > 2 AND id < 10");
+            while (rs.next()) {
+                System.out.println("Номер в выборке: " + rs.getRow() + "Номер в базе: " + rs.getInt("id" +
+                        "Имя: " + rs.getString("firstname")));
+                connection.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
